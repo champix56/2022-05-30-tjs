cc5d7b5715aff70d290a4f15b6ff562935710384
champix56 <desorbaix@free.fr>
2022-05-30 12:04:47 +0100

    reformulation principal + vidange projet

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //comment js
+  /*comment multilignes js*/
+  return (
+    <div className="App">
+      {/*comment dans XML*/}
+      DEMAT BREIZH
+    </div>
+  );
+}
+
+export default App;
