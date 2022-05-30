b23fd8e7e23d8be1838cfc9e2f8a3c91ead20d5e
champix56 <desorbaix@free.fr>
2022-05-30 14:55:40 +0100

    reformat prettier

--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-
+import React from "react";
+import PropTypes from "prop-types";
 function Button(props) {
-    console.log(props)
-    return <button type={props.type}>{props.children}</button>
+  console.log(props);
+  return <button type={props.type}>{props.children}</button>;
 }
+
+
 export default Button;
