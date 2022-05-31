b73b37b11947aae5b2d5525941bab290ae57a741
champix56 <desorbaix@free.fr>
2022-05-31 12:25:06 +0100

    config de l'outil generate-react-cli

---
+++ grc_templates/func_hook/TemplateName.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import style from "./TemplateName.module.css";
+const initialState = {};
+const TemplateName = (props) => {
+  const [state, setstate] = useState(initialState);
+  useEffect(() => {
+    console.log("TemplateName monté");
+  }, []);
+  return (
+    <div className={style.TemplateName} data-testid="TemplateName">
+      templateName
+    </div>
+  );
+};
+
+TemplateName.propTypes = {};
+
+export default TemplateName;
