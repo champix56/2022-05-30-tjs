b9385d29feaeef5045f084191d0c1f5dbc77f2f4
champix56 <desorbaix@free.fr>
2022-05-31 14:24:06 +0100

    fin de gen des cmp necessaires

---
+++ src/App/components/ui/MemeForm/MemeForm.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import style from "./MemeForm.module.css";
+const initialState = {};
+const MemeForm = (props) => {
+  const [state, setstate] = useState(initialState);
+  useEffect(() => {
+    console.log("MemeForm monté");
+  }, []);
+  return (
+    <div className={style.MemeForm} data-testid="MemeForm">
+      memeForm
+    </div>
+  );
+};
+
+MemeForm.propTypes = {};
+
+export default MemeForm;
