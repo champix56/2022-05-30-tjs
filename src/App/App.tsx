61df1505daabf8ee89e294a97a59ced501a2e6ba
champix56 <desorbaix@free.fr>
2022-05-31 10:09:08 +0100

    manip d'etat class chez lenfant

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Button from "./components/Button/Button";
+interface IAppState{
+  message?:'hello'|'goodbye',
+  counter:number,
+}
+interface IAppProps{}
 
-class App extends React.Component {
+class App extends React.Component<IAppProps,IAppState> {
+
+  constructor(props:IAppProps){
+    super(props);
+    this.state={message:'hello',counter:0}
+  }
   render() {
     return (
       <div className="App" style={{ textAlign: "center" }}>
-        Valeur du compteur : 1
+        Valeur du compteur : {this.state.counter}
         <hr />
-        <Button bgColor="tomato">decrem -1</Button>
+        <Button action={()=>{
+          this.setState({counter:this.state.counter-1})
+          console.log(this.state.counter)
+        }} bgColor="tomato">decrem -1</Button>
         <Button bgColor="skyblue">increm +1</Button>
       </div>
     );
