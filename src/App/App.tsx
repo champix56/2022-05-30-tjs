d3dcc69ac4d2dc911e1c20b2ff15f3cab62aed49
champix56 <desorbaix@free.fr>
2022-06-01 16:25:01 +0100

    fix after connect

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC<IAppProps> = (props) => {
       <Header />
       <Navbar />
       <FlexLayout>
-        <ConnectedMemeSVG image={undefined} />
+        <ConnectedMemeSVG/>
         <ConnectedMemeForm />
       </FlexLayout>
       <Footer />
