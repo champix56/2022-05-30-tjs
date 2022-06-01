1c07e932bedcb02f6cf3f9e5f6f466cdd3bd2cc3
champix56 <desorbaix@free.fr>
2022-06-01 09:17:54 +0100

    gestion map des listes

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -17,10 +17,16 @@ interface IAppState {
 }
 interface IAppProps {}
 
+const images: Array<ImageInterface> = [
+  { id: 0, name: "goudig", w: 500, h: 500, url: "goudigFull_500x500.jpg" },
+  { id: 1, name: "quimper", w: 418, h: 348, url: "quimper_418_348.gif" },
+  { id: 2, name: "tabarly", w: 655, h: 437, url: "tabarly_655x437.jpg" },
+];
+
 class App extends React.Component<IAppProps, IAppState> {
   constructor(props: IAppProps) {
     super(props);
-    this.state = { currentMeme: emptyMeme, images: [] };
+    this.state = { currentMeme: emptyMeme, images: images };
   }
   render() {
     return (
@@ -28,9 +34,12 @@ class App extends React.Component<IAppProps, IAppState> {
         <Header />
         <Navbar />
         <FlexLayout>
-          <MemeSVGViewer image={undefined} meme={this.state.currentMeme} />
-          <MemeForm
+          <MemeSVGViewer image={
+            this.state.images.find(e=>e.id===this.state.currentMeme.imageId)
+          } meme={this.state.currentMeme} basePath='/media/' />
+          <MemeForm 
             meme={this.state.currentMeme}
+            images={this.state.images}
             onMemeChange={(meme: MemeInterface) => {
               this.setState({ currentMeme: meme });
             }}
