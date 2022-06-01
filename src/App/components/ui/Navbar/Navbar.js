ade6fd8dcce95ca6311cc23ba5ce69abe75e8108
champix56 <desorbaix@free.fr>
2022-06-01 18:04:08 +0100

    ajout des linkcontainer pour beauté css des liens bs

--- src/App/components/ui/Navbar/Navbar.js
+++ src/App/components/ui/Navbar/Navbar.js
@@ -1,24 +1,34 @@
 import React from "react";
 import style from "./Navbar.module.css";
 import { Container, Nav, Navbar as NavBar } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.css'
-import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 const Navbar = (props) => {
   return (
     <div className={style.Navbar} data-testid="Navbar">
       <NavBar bg="primary" variant="dark">
         <Container>
-          <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+          <LinkContainer to="/">
+            <NavBar.Brand>Navbar</NavBar.Brand>
+          </LinkContainer>
           <Nav className="me-auto">
-            <Link to="/">Home</Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/editor">
+              <Nav.Link>new Meme</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/thumbnail">
+              <Nav.Link>Thumbnail</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/editor/0">
+              <Nav.Link>edit id 0</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Container>
       </NavBar>
     </div>
   );
 };
-
 
 export default Navbar;
